dd47b040de035a7762a78e13dc187d3096214d78
Rafał Filipek <rafal.filipek@gmail.com>
2018-10-04 11:55:26 +0200

    Custom transition
    Add view with custom transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import { Transition, config, animated } from 'react-spring';
-import NProgress from 'nprogress';
-import Router from 'next/router';
+import React from "react";
+import App, { Container } from "next/app";
+import { Transition, config, animated } from "react-spring";
+import NProgress from "nprogress";
+import Router from "next/router";
 
-Router.events.on('routeChangeStart', url => {
+Router.events.on("routeChangeStart", url => {
   console.log(`Loading: ${url}`);
   NProgress.start();
 });
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -22,29 +22,36 @@ export default class MyApp extends App {
 
   render() {
     const { Component, pageProps, router } = this.props;
+    console.log(pageProps);
     return (
       <Container>
         <Transition
           keys={[router.asPath]}
           native
-          from={{
-            opacity: 0,
-            position: 'absolute',
-            transform: 'translateY(100px)',
-          }}
-          enter={{
-            opacity: 1,
-            position: 'static',
-            transform: 'translateY(0px)',
-          }}
-          leave={{
-            opacity: 0,
-            position: 'absolute',
-            transform: 'translateY(-100px)',
-          }}
+          from={
+            pageProps.transitionLeave || {
+              opacity: 0,
+              position: "absolute",
+              transform: "translateY(100px)"
+            }
+          }
+          enter={
+            pageProps.transitionEnter || {
+              opacity: 1,
+              position: "static",
+              transform: "translateY(0px)"
+            }
+          }
+          leave={
+            pageProps.transitionLeave || {
+              opacity: 0,
+              position: "absolute",
+              transform: "translateY(-100px)"
+            }
+          }
         >
           {styles => (
-            <animated.div style={{ width: '100vw', ...styles }}>
+            <animated.div style={{ width: "100vw", ...styles }}>
               <Component {...pageProps} />
             </animated.div>
           )}
